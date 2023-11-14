58c0c70615316be30a75743ce833a149dd5ce3aa
Erika-Santana <erika_alves_10@outlook.com>
2023-11-14 11:11:12 -0300

    Atualziacao

--- exercicios_fixacao/exercicios_c.c/erikaSantana_3EX.c
+++ exercicios_fixacao/exercicios_c.c/erikaSantana_3EX.c
@@ -1,50 +1,83 @@
-/*Exercício 3: Crie um programa que simule uma calculadora simples que pode realizar operações de soma, subtração, multiplicação e divisão.
+    /*Exercício 3: Crie um programa que simule uma calculadora simples que pode realizar operações de soma, subtração, multiplicação e divisão.
 
-·        Use uma estrutura typedef para representar uma operação, que inclui dois operandos e um operador.
+    ·        Use uma estrutura typedef para representar uma operação, que inclui dois operandos e um operador.
 
-·        Utilize alocação dinâmica para armazenar as operações e permitir que o usuário insira várias operações.*/
+    ·        Utilize alocação dinâmica para armazenar as operações e permitir que o usuário insira várias operações.*/
 
 
-#include <stdio.h>
+    #include <stdio.h>
+    #include <stdlib.h>
 
-typedef struct{
-    char operando[30];
-    double operador1;
-    double operador2;
+    typedef struct{
+        char operando[30];
+        double operador1;
+        double operador2;
 
-}Operacao;
+    }Operacao;
 
-int main(int argc, char const *argv[])
-{
-    int **ptrJogador;
-    int valor = 0;
-    int *jogador = (Operacao *)malloc(1 *sizeof(Operacao));
-    ptrJogador = &jogador;
-
-    while (valor != 5)
-    { 
-        printf("--------------------------------\n");
-        printf("        CALCULADORA             \n");
-        printf("--------------------------------\n");
-        printf("[1] MULTIPLICAÇÃO    -----------\n");
-        printf("[2] ADIÇÃO           -----------\n");
-        printf("[3] SUBTRAÇÃO        -----------\n");
-        printf("[4] DIVISÃO          -----------\n");
-        printf("--------------------------------\n");
-
-        printf("DIGITE A CONTA A SER FEITA: ");
-        scanf("%d", &valor);
-
-        switch (valor)
-        {
-        case 1:
-            
-            break;
+    int main(int argc, char const *argv[])
+    {
+    
+        int valor = 0, opcao = 0;
+        double resultado = 0.0;
+        printf("Digite quantas operações deseja fazer: ");
+        scanf("%d", &opcao);
+        Operacao *jogador = (Operacao *)malloc(opcao *sizeof(Operacao));
         
-        default:
+
+    for (int i = 0; i < opcao; i++)
+    {
+        printf("--------------------------------\n");
+            printf("        CALCULADORA             \n");
+            printf("--------------------------------\n");
+            printf("[1] MULTIPLICAÇÃO    -----------\n");
+            printf("[2] ADIÇÃO           -----------\n");
+            printf("[3] SUBTRAÇÃO        -----------\n");
+            printf("[4] DIVISÃO          -----------\n");
+            printf("--------------------------------\n");
+
+            printf("DIGITE A CONTA A SER FEITA: ");
+            scanf("%d", &valor);
+
+            printf("DIGITE O VALOR DO PRIMEIRO: \n");
+                scanf(" %lf", &jogador[i].operador1);
+                printf("DIGITE O VALOR DO SEGUNDO: \n");
+                scanf(" %lf", &jogador[i].operador2);
+
+            switch (valor)
+            {
+                
+            case 1:
+                resultado = (jogador[i].operador1) * (jogador[i].operador2);
+
+                printf("O resultado é: %.2lf\n", resultado);           
+                break;
+            case 2:
+                resultado = (jogador[i].operador1) + (jogador[i].operador2);
+                printf("O resultado é: %.2lf\n", resultado);           
+                break;
+            case 3:
+                resultado = (jogador[i].operador1) - (jogador[i].operador2);
+                printf("O resultado é: %.2lf\n", resultado);     
+
+            case 4:
+
+            if (jogador[i].operador2 == 0)
+            {
+                printf("VALOR INVÁLIDO!");
+            }else{
+                resultado = (jogador[i].operador1) / (jogador[i].operador2);
+                printf("O resultado é: %.2lf\n", resultado);    
+            }
+    
             break;
+            
+            default:
+                break;
+            }
+            return 0;
         }
-        return 0;
+
     }
-   
-}
+
+
--- exercicios_fixacao/exercicios_c.c/erikaSantana_5EX.c
+++ exercicios_fixacao/exercicios_c.c/erikaSantana_5EX.c
@@ -34,3 +34,20 @@ int main(int argc, char const *argv[])
 }
 
 
+/*Exercício 5: Crie um jogo de "Adivinhe a Palavra" em que o jogador tenta adivinhar uma palavra oculta.
+
+·        A palavra oculta é escolhida aleatoriamente e o jogador deve adivinhar as letras da palavra uma por uma.
+
+·        O jogador tem um número limitado de tentativas para adivinhar a palavra.
+
+·        Use alocação dinâmica para armazenar a palavra oculta e as letras adivinhadas.
+
+·        O programa deve exibir a palavra oculta com letras em branco e revelar letras corretas à medida que o jogador adivinha.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
